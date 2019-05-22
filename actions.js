7f5521a4cac8087a5067076d436d1557b581d06d
els <navesllanes@gmail.com>
2019-05-22 16:24:00 +0100

    Added background picture and fonts

--- actions.js
+++ actions.js
@@ -1,2 +1,28 @@
+  $(document).ready(function(){
+                                        $("#Repository1").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/A-_Java", "_blank").focus();
+
+                                                });
+                                        $("#Repository2").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/GeneticAlgorithm", "_blank").focus();
+
+                                                });
+                                        $("#Repository3").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/RandomPythonScripts", "_blank").focus();
+
+                                                });
+                                        $("#Repository4").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/Google_Hashcode_2018_Solution", "_blank").focus();
+
+                                                });
+
+
+
+
+                                });
 
 
