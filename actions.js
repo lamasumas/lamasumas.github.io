bb7a4b2b0926c9e7b408296e2d56f6074cb3509c
els <navesllanes@gmail.com>
2019-09-16 19:30:56 +0200

    sdfj

--- actions.js
+++ actions.js
@@ -1,20 +1,30 @@
   $(document).ready(function(){
                                         $("#Repository1").click(
                                                 function() {
-                                                        window.open("https://github.com/lamasumas/A-_Java", "_blank").focus();
+                                                        window.open("https://github.com/lamasumas/ECCO", "_blank").focus();
 
                                                 });
                                         $("#Repository2").click(
                                                 function() {
-                                                        window.open("https://github.com/lamasumas/GeneticAlgorithm", "_blank").focus();
+                                                        window.open("https://github.com/lamasumas/ECCO_Data_Admin", "_blank").focus();
 
                                                 });
                                         $("#Repository3").click(
                                                 function() {
-                                                        window.open("https://github.com/lamasumas/RandomPythonScripts", "_blank").focus();
+                                                        window.open("https://github.com/lamasumas/A-_Java", "_blank").focus();
 
                                                 });
                                         $("#Repository4").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/Machine_Learning", "_blank").focus();
+
+                                                });
+                                        $("#Repository5").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas/IrlCPC2018", "_blank").focus();
+
+                                                });
+                                        $("#Repository6").click(
                                                 function() {
                                                         window.open("https://github.com/lamasumas/Google_Hashcode_2018_Solution", "_blank").focus();
 
