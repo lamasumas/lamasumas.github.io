a21aaf32b3339a81a6b741e6c41bf24914ff8848
els <navesllanes@gmail.com>
2019-11-06 19:31:09 +0100

    SolucionadoBug de personalBacgroung y añadidiconos

--- actions.js
+++ actions.js
@@ -29,7 +29,10 @@
                                                         window.open("https://github.com/lamasumas/Google_Hashcode_2018_Solution", "_blank").focus();
 
                                                 });
-
+                                        $(".fa-linkedin").click( x =>  window.open("https://www.linkedin.com/in/eduardo-lamas-su%C3%A1rez-568b72171/", "_blank").focus());
+                                        $(".fa-github-square").click( x =>  window.open("https://github.com/lamasumas", "_blank").focus());
+                                        $(".fa-bitbucket").click( x =>  window.open("https://bitbucket.org/%7Ba64c1405-4267-441c-93a3-489cf8d269d5%7D/", "_blank").focus());
+                                        $(".fa-twitter-square").click( x =>  window.open("https://twitter.com/Lamasumas1", "_blank").focus());
 
 
 
