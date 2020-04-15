ffe4149457cdeb0816d21797ec19a829ebf95393
els <navesllanes@gmail.com>
2020-04-15 19:06:08 +0200

    bug fixed

--- Solid/actions.js
+++ Solid/actions.js
@@ -1,14 +1,35 @@
   $(document).ready(function(){
-                                        $("#Repository1").click(
+                                        $("#member1").click(
                                                 function() {
-                                                        window.open("https://github.com/lamasumas/ECCO", "_blank").focus();
+                                                        window.open("https://github.com/alvarezGarciaMarcos", "_blank").focus();
 
                                                 });
-                                        $(".fa-linkedin").click( x =>  window.open("https://www.linkedin.com/in/eduardo-lamas-su%C3%A1rez-568b72171/", "_blank").focus());
-                                        $(".fa-github-square").click( x =>  window.open("https://github.com/lamasumas", "_blank").focus());
-                                        $(".fa-bitbucket").click( x =>  window.open("https://bitbucket.org/%7Ba64c1405-4267-441c-93a3-489cf8d269d5%7D/", "_blank").focus());
-                                        $(".fa-twitter-square").click( x =>  window.open("https://twitter.com/Lamasumas1", "_blank").focus());
+                                        $("#member2").click(
+                                                function() {
+                                                        window.open("https://github.com/themrcesi", "_blank").focus();
 
+                                        });
+                                        $("#member3").click(
+                                                function() {
+                                                        window.open("https://github.com/InigoGutierrez", "_blank").focus();
+                
+                                        });
+                                        $("#member4").click(
+                                                function() {
+                                                        window.open("https://github.com/alvarogarinf", "_blank").focus();
+
+                                                });
+                                        $("#member5").click(
+                                                function() {
+                                                        window.open("https://github.com/garciaAlvarezPelayo", "_blank").focus();
+
+                                        });
+                                        $("#member6").click(
+                                                function() {
+                                                        window.open("https://github.com/lamasumas", "_blank").focus();
+
+                                        });
+                                 
 
 
                                 });
