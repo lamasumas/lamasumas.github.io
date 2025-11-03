5ad6278c73646c1fd5214aab3347c77c2cf467d4
els <navesllanes@gmail.com>
2025-11-03 22:34:06 +0900

    feat: about and side menu

--- actions.js
+++ actions.js
@@ -1,41 +1,61 @@
-  $(document).ready(function(){
-                                        $("#Repository1").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/ECCO", "_blank").focus();
-
-                                                });
-                                        $("#Repository2").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/ECCO_Data_Admin", "_blank").focus();
-
-                                                });
-                                        $("#Repository3").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/A-_Java", "_blank").focus();
-
-                                                });
-                                        $("#Repository4").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/Machine_Learning", "_blank").focus();
-
-                                                });
-                                        $("#Repository5").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/TFG_Muestreo_Fauna", "_blank").focus();
-
-                                                });
-                                        $("#Repository6").click(
-                                                function() {
-                                                        window.open("https://github.com/lamasumas/Google_Hashcode_2018_Solution", "_blank").focus();
-
-                                                });
-                                        $(".fa-linkedin").click( x =>  window.open("https://www.linkedin.com/in/eduardo-lamas-su%C3%A1rez-568b72171/", "_blank").focus());
-                                        $(".fa-github-square").click( x =>  window.open("https://github.com/lamasumas", "_blank").focus());
-                                        $(".fa-bitbucket").click( x =>  window.open("https://bitbucket.org/%7Ba64c1405-4267-441c-93a3-489cf8d269d5%7D/", "_blank").focus());
-                                        $(".fa-twitter-square").click( x =>  window.open("https://twitter.com/Lamasumas1", "_blank").focus());
-                                        $(".fa-google-play").click( x =>  window.open("https://play.google.com/store/apps/developer?id=Lamasumas&hl=es&gl=US", "_blank").focus());
-
-
-                                });
-
-
+$(document).ready(function() {
+    $("#Repository1").click(function() {
+        window.open("https://github.com/lamasumas/ECCO", "_blank").focus();
+    });
+    $("#Repository2").click(function() {
+        window
+            .open("https://github.com/lamasumas/ECCO_Data_Admin", "_blank")
+            .focus();
+    });
+    $("#Repository3").click(function() {
+        window.open("https://github.com/lamasumas/A-_Java", "_blank").focus();
+    });
+    $("#Repository4").click(function() {
+        window
+            .open("https://github.com/lamasumas/Machine_Learning", "_blank")
+            .focus();
+    });
+    $("#Repository5").click(function() {
+        window
+            .open("https://github.com/lamasumas/TFG_Muestreo_Fauna", "_blank")
+            .focus();
+    });
+    $("#Repository6").click(function() {
+        window
+            .open(
+                "https://github.com/lamasumas/Google_Hashcode_2018_Solution",
+                "_blank",
+            )
+            .focus();
+    });
+    $(".fa-linkedin").click((x) =>
+        window
+            .open(
+                "https://www.linkedin.com/in/eduardo-lamas-su%C3%A1rez-568b72171/",
+                "_blank",
+            )
+            .focus(),
+    );
+    $(".fa-github-square").click((x) =>
+        window.open("https://github.com/lamasumas", "_blank").focus(),
+    );
+    $(".fa-bitbucket").click((x) =>
+        window
+            .open(
+                "https://bitbucket.org/%7Ba64c1405-4267-441c-93a3-489cf8d269d5%7D/",
+                "_blank",
+            )
+            .focus(),
+    );
+    $(".fa-twitter-square").click((x) =>
+        window.open("https://twitter.com/Lamasumas1", "_blank").focus(),
+    );
+    $(".fa-google-play").click((x) =>
+        window
+            .open(
+                "https://play.google.com/store/apps/developer?id=Lamasumas&hl=es&gl=US",
+                "_blank",
+            )
+            .focus(),
+    );
+});
